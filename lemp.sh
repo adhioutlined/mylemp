e0864f4f888fd1c9425676d963302881afa8cada
adhioutlined <adhi.pri@gmail.com>
2022-07-12 18:52:51 +0700

    initial

---
+++ lemp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Text Colour
+RED="\033[01;31m"    # Issues/Errors
+GREEN="\033[01;32m"  # Success
+YELLOW="\033[01;33m" # Warnings/Information
+BLUE="\033[01;34m"   # Heading
+BOLD="\033[01;01m"   # Highlight
+RESET="\033[00m"     # Normal
+
+# Check if running as root
+if [[ "${EUID}" -ne 0 ]]; then
+    echo -e "${RED}[!]${RESET} This script must be ${RED}run as root${RESET}" 1>&2
+    exit 1
+fi
